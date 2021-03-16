dca98d5ac321138043ef3056103abde86f4fe752
Hammad Khalid <hammaddotkhalid101@gmail.com>
2021-03-16 16:16:21 -0400

    Created the App and Root component

--- frontend/hellostocks.jsx
+++ frontend/hellostocks.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './util/session_api_util'
+import Root from './components/root'
 import configureStore from './store/store';
+import { signup, login, logout } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     const store = configureStore();
+
+    // Testing 
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    ReactDOM.render(<h1>Welcome to HelloStocks!</h1>, root)
+    // Testing 
+
+    ReactDOM.render(<Root store={store} />, root)
 })
 
+//Testing
 window.signup = signup;
 window.login = login;
 window.logout = logout;
+// Testing
